24a45895476ea2f4aabd45908a4172c0fb8b6d9a
QaseemLodhi <qaseem@marketlytics.com>
2017-10-27 14:36:22 +0500

    animated-list
    animated whole guess list

--- src/view.js
+++ src/view.js
@@ -1,9 +1,9 @@
-import {addEvent} from "./helpers";
+import { addEvent } from "./helpers";
 
 import './styles/game-view.scss';
 
-export default class View{
-	constructor(){
+export default class View {
+	constructor() {
 		console.log("View loaded");
 
 		this.guessInput = document.getElementById('guessInput');
@@ -12,28 +12,29 @@ export default class View{
 		this.gameViewContainer = document.getElementById('game-view-container');
 		this.userGuesses = document.getElementById('user-guesses');
 		this.gameMessage = document.getElementById('game-message');
+		this.pre = [];
+
 	}
 
 	/**
 	 * Sets the game view, should also set the correct pieces of the hangman
 	 */
-	setGameView(){
+	setGameView() {
 		let img = document.createElement('img');
 		img.setAttribute('src', require('./assets/img/6.jpg'));
 		img.id = 'game-view';
 		this.gameViewContainer.appendChild(img);
 		this.guessInput.maxLength = 1;
-		
+
 	}
 	/**
 	 * This displays the secret word as underscores. One underscore per letter.
 	 * @param {Array} secretWord
 	 */
-	setSecretWordDisplay(secretWord){
-		console.log("this view setSecretWord:", secretWord);
+	setSecretWordDisplay(secretWord) {
 		let letterPlaces = '_';
 
-		for(let i = 1; i < secretWord.length; i++){
+		for (let i = 1; i < secretWord.length; i++) {
 			letterPlaces += ' _';
 		}
 
@@ -47,7 +48,7 @@ export default class View{
 	 * @param {string} gameUpdate.remainingGuesses - How many more times user can guess
 	 * @param {Array} gameUpdate.userGuesses - Contains all of the user guesses
 	 */
-	updateSecretWordDisplay(secretWordUpdate, gameUpdate){
+	updateSecretWordDisplay(secretWordUpdate, gameUpdate) {
 		this.secretWordContainer.innerHTML = secretWordUpdate;
 		console.log("and the game status is..", gameUpdate);
 		this.updateUserGuessDisplay(gameUpdate.userGuesses);
@@ -57,8 +58,18 @@ export default class View{
 	 * Displays and updates the user guesses on screen
 	 * @param userGuesses
 	 */
-	updateUserGuessDisplay(userGuesses){
-		this.userGuesses.innerHTML = `Your guesses so far: <span class='bold'>${userGuesses}</span>`;
+	updateUserGuessDisplay(userGuesses) {
+		if (this.pre.length === 0 || userGuesses.length > this.pre.length) {
+			if(this.userGuesses.classList.contains('animated','wobble')){
+				this.userGuesses.classList.remove('animated','wobble');	
+			}
+			setTimeout(()=>{
+			this.userGuesses.innerHTML = `Your guesses so far: <span class='bold'>${userGuesses}</span>`;
+			this.pre.push(userGuesses);
+			this.userGuesses.classList.add('animated', 'wobble');
+			},200)
+		}
+
 	}
 
 	/**
@@ -66,10 +77,10 @@ export default class View{
 	 * @param {Function} handler
 	 */
 
-	bindSaveUserGuess(handler){
-		addEvent(this.guessSubmit, 'click', (e) =>{
+	bindSaveUserGuess(handler) {
+		addEvent(this.guessSubmit, 'click', (e) => {
 			const guessedLetter = this.guessInput.value.trim();
-			if(guessedLetter){
+			if (guessedLetter) {
 				handler(this.guessInput.value);
 			} else {
 				console.log("no value submitted");
