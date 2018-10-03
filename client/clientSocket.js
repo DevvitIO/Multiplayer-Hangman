de460d8132d45bb340d63e373ccd591c4d63b67f
Michael Ney <ney.quentin.michael@gmail.com>
2018-10-03 12:41:06 -0700

    Build error fix

--- client/clientSocket.js
+++ client/clientSocket.js
@@ -1,12 +1,22 @@
+var io = require("socket.io-client");
 var socket = io('http://localhost:5000');
 
-document.getElementById('guess-submit').addEventListener('click', function(){
-    submitGuess(document.getElementById('guessInput').value);
-    document.getElementById('guessInput').value = '';
+let gameMessage = document.getElementById('game-message');
+let secretWord = document.getElementById('secret-word-container');
+let userGuesses = document.getElementById('user-guesses');
+let onlinePlayers = document.getElementById('onlinePlayers');
+let guessInput = document.getElementById('guessInput');
+let usernameInput = document.getElementById('usernameInput');
+let guessSubmit = document.getElementById('guess-submit');
+let usernameSubmit = document.getElementById('username-submit');
+
+guessSubmit.addEventListener('click', function(){
+    submitGuess(guessInput.value);
+    guessInput.value = '';
 });
 
-document.getElementById('username-submit').addEventListener('click', function(){
-    setUsername(document.getElementById('usernameInput').value);
+usernameSubmit.addEventListener('click', function(){
+    setUsername(usernameInput.value);
 });
 
 function setUsername(username) {
@@ -20,12 +30,12 @@ function submitGuess(letter) {
 }
 
 function updateGameState(data) {
-	document.getElementById('secret-word-container').innerHTML = data.blankword;
-	document.getElementById('user-guesses').innerHTML = 'Guesses: ' + data.guesses;
+	secretWord.innerHTML = data.blankword;
+	userGuesses.innerHTML = 'Guesses: ' + data.guesses;
 }
 
 socket.on('playersOnline', function(count){
-	document.getElementById('onlinePlayers').innerHTML = count;
+	onlinePlayers.innerHTML = count;
 });
 
 socket.on('gameInformation', (data) => {
@@ -33,32 +43,32 @@ socket.on('gameInformation', (data) => {
 });
 
 socket.on('repeatGuess', (data) => {
-	document.getElementById('game-message').innerHTML = "That letter has already been guessed. Try again!";
+	gameMessage.innerHTML = "That letter has already been guessed. Try again!";
 });
 
 socket.on('incorrectGuess', (data) => {
 	console.log(data);
 	updateGameState(data);
-	document.getElementById('game-message').innerHTML = data.guesser + ' guessed incorrectly. There are ' + (6 - data.incorrect) + ' guesses left.';
+	gameMessage.innerHTML = data.guesser + ' guessed incorrectly. There are ' + (6 - data.incorrect) + ' guesses left.';
 });
 
 socket.on('correctGuess', (data) => {
 	console.log(data);
 	updateGameState(data);
-	document.getElementById('game-message').innerHTML = data.guesser + ' guessed correctly!';
+	gameMessage.innerHTML = data.guesser + ' guessed correctly!';
 });
 
 socket.on('victory', (data) => {
 	updateGameState(data);
-	document.getElementById('game-message').innerHTML = '<span style="color: green">' + data.guesser + ' guessed correctly to win the game! Victory!</span>';
+	gameMessage.innerHTML = '<span style="color: green">' + data.guesser + ' guessed correctly to win the game! Victory!</span>';
 });
 
 socket.on('gameOver', (data) => {
 	updateGameState(data);
-	document.getElementById('game-message').innerHTML = '<span style="color: red">' + data.guesser + ' guessed wrong. Game Over!</span>';
+	gameMessage.innerHTML = '<span style="color: red">' + data.guesser + ' guessed wrong. Game Over!</span>';
 });
 
 socket.on('newGame', (data) => {
 	updateGameState(data);
-	document.getElementById('game-message').innerHTML = 'New game has started!';
+	gameMessage.innerHTML = 'New game has started!';
 });
